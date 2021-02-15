a48285526a1ec2119b493f0a54da2c19b54f0611
Tristan Pacba <tpacba@live.com>
2021-02-15 15:20:45 -0500

    used pandas.DataFrame to filter tabular results

--- main.py
+++ main.py
@@ -26,24 +26,26 @@ if __name__ == "__main__":
 
     data = r.json()
 
-    print(data)
+    # print(data)
 
-    # song_names = []
-    # artist_names = []
-    # played_at_list = []
-    # timestamps = []
+    song_names = []
+    artist_names = []
+    played_at_list = []
+    timestamps = []
 
-    # for song in data["items"]:
-    #     song_names.append(song["track"]["name"])
-    #     artist_names.append(song["track"]["album"]["artists"][0]["name"])
-    #     played_at_list.append(song["played_at"])
-    #     timestamps.append(song["played_at"][0:10])
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at_list.append(song["played_at"])
+        timestamps.append(song["played_at"][0:10])
 
-    # song_dict = {
-    #     "song_name": song_names,
-    #     "artist_name": artist_names,
-    #     "played_at": played_at_list,
-    #     "timestamp": timestamps
+    song_dict = {
+        "song_name": song_names,
+        "artist_name": artist_names,
+        "played_at": played_at_list,
+        "timestamp": timestamps
+    }
 
-    # }
+    song_df = pandas.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
 
+    print(song_df)
